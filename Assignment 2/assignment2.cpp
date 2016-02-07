fdcc6ab5fde200f6ea3cd9566eb9ccfb110e6382
MichaelReiter <michaelreiter@live.ca>
2016-02-07 13:48:46 -0800

    Prepare to handle cursor movement

--- Assignment 2/assignment2.cpp
+++ Assignment 2/assignment2.cpp
@@ -17,15 +17,15 @@ using namespace std;
 
 static const int WINDOW_SIZE_X = 800;
 static const int WINDOW_SIZE_Y = 600;
-static float BALL_VELOCITY = 150; // velocity is in pixels/second
+static float CURSOR_VELOCITY = 150; // velocity is in pixels/second
 
-class A2Canvas{
+class A2Canvas {
 public:
 	static const int CANVAS_SIZE_X = 800;
 	static const int CANVAS_SIZE_Y = 600;
-	static const int BALL_RADIUS = 15;
+	static const int CURSOR_RADIUS = 15;
 
-	A2Canvas(){
+	A2Canvas() {
 		ball_position.x = CANVAS_SIZE_X/2;
 		ball_position.y = CANVAS_SIZE_Y/2;
 		ball_direction.x = cos(30*(M_PI/180.0));
@@ -75,17 +75,19 @@ public:
 	}
 private:
 	void handle_key_down(SDL_Keycode key) {
-		if (key == SDLK_r) {
-			Vector2d rotation( cos(30*(M_PI/180)), sin(30*(M_PI/180)) );
-			Vector2d new_direction(
-				rotation.x*ball_direction.x - rotation.y*ball_direction.y,
-				rotation.x*ball_direction.y + rotation.y*ball_direction.x);
-			ball_direction = new_direction;
+		if (key == SDLK_UP) {
+
+		} else if (key == SDLK_DOWN) {
+
+		} else if (key == SDLK_LEFT) {
+
+		} else if (key == SDLK_RIGHT) {
+
 		}
 	}
 
 	void handle_mouse_down(int x, int y, int button) {
-		BALL_VELOCITY *= 2;
+		CURSOR_VELOCITY *= 2;
 	}
 
 	void handle_mouse_up(int x, int y, int button) {
@@ -97,43 +99,42 @@ private:
 	void draw(SDL_Renderer *renderer, float frame_delta_ms) {
 	
 		float frame_delta_seconds = frame_delta_ms/1000.0;
-
-		float position_delta = frame_delta_seconds*BALL_VELOCITY;
+		float position_delta = frame_delta_seconds * CURSOR_VELOCITY;
 
 		Vector2d new_position = ball_position + position_delta*ball_direction;
 
-		//The ball collides with the edge of the screen if the new position is less than BALL_RADIUS
+		//The ball collides with the edge of the screen if the new position is less than CURSOR_RADIUS
 		//pixels away from any edge.
 
-		if (new_position.x <= BALL_RADIUS) {
+		if (new_position.x <= CURSOR_RADIUS) {
 			//Collide with left edge
 			
 			//Determine how far past the collision point the new position is.
-			float offset_x = BALL_RADIUS-new_position.x;
+			float offset_x = CURSOR_RADIUS-new_position.x;
 			//Mirror the direction around the y axis (since the ball bounces)
 			ball_direction.x = -ball_direction.x;
 			new_position.x += 2*offset_x;
-		} else if (new_position.x >= CANVAS_SIZE_X - BALL_RADIUS) {
+		} else if (new_position.x >= CANVAS_SIZE_X - CURSOR_RADIUS) {
 			//Collide with right edge
 
 			//Determine how far past the collision point the new position is.
-			float offset_x = new_position.x - (CANVAS_SIZE_X-BALL_RADIUS);
+			float offset_x = new_position.x - (CANVAS_SIZE_X - CURSOR_RADIUS);
 			//Mirror the direction around the y axis (since the ball bounces)
 			ball_direction.x = -ball_direction.x;
 			new_position.x -= 2*offset_x;
-		} else if(new_position.y <= BALL_RADIUS) {
+		} else if(new_position.y <= CURSOR_RADIUS) {
 			//Collide with top
 
 			//Determine how far past the collision point the new position is.
-			float offset_y = BALL_RADIUS-new_position.y;
+			float offset_y = CURSOR_RADIUS-new_position.y;
 			//Mirror the direction around the x axis (since the ball bounces)
 			ball_direction.y = -ball_direction.y;
 			new_position.y += 2*offset_y;
-		} else if(new_position.y >= CANVAS_SIZE_Y - BALL_RADIUS) {
+		} else if(new_position.y >= CANVAS_SIZE_Y - CURSOR_RADIUS) {
 			//Collide with bottom
 
 			//Determine how far past the collision point the new position is.
-			float offset_y = new_position.y - (CANVAS_SIZE_Y-BALL_RADIUS);
+			float offset_y = new_position.y - (CANVAS_SIZE_Y - CURSOR_RADIUS);
 			//Mirror the direction around the x axis (since the ball bounces)
 			ball_direction.y = -ball_direction.y;
 			new_position.y -= 2*offset_y;
@@ -144,7 +145,7 @@ private:
 		SDL_RenderClear(renderer);
 	
 		const ColourRGB& ball_colour = ColourRGB(0,0,0);
-		filledCircleRGBA(renderer, ball_position.x, ball_position.y, BALL_RADIUS, ball_colour.r, ball_colour.g, ball_colour.b, 255);
+		filledCircleRGBA(renderer, ball_position.x, ball_position.y, CURSOR_RADIUS, ball_colour.r, ball_colour.g, ball_colour.b, 255);
 
 		SDL_RenderPresent(renderer);
 	}
@@ -152,15 +153,15 @@ private:
 };
 
 int main() {
-	SDL_Window* window = SDL_CreateWindow("CSC 205 A2",
+	SDL_Window* window = SDL_CreateWindow("CSC 205 Assignment 2",
     SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
     WINDOW_SIZE_X, WINDOW_SIZE_Y, 
 	  SDL_WINDOW_SHOWN);
 
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0/*SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED*/);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
 
-	//Initialize the canvas to solid green
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	// Initialize the canvas to solid black
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
 	SDL_RenderPresent(renderer);
 	
