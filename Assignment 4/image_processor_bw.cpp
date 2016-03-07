efcd2cb28556350b5f72034a05af87fbac68497b
MichaelReiter <michaelreiter@live.ca>
2016-03-07 11:00:42 -0800

    Add gaussian cdf

--- Assignment 4/image_processor_bw.cpp
+++ Assignment 4/image_processor_bw.cpp
@@ -10,10 +10,32 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <math.h>
 #include "lodepng.h"
 #include "png_canvas.h"
 
 using namespace std;
+
+double gaussian_pdf(double x, double mean = 128, double stddev = 50) {
+	return (1.0 / (stddev * sqrt(2*M_PI))) * exp(-0.5 * pow((x - mean)/stddev, 2.0));
+}
+
+
+vector<double> gaussian_cdf(double x, double mean = 128, double stddev = 50) {
+	vector<double> histogram(256);
+
+	for (int i = 0; i < histogram.size(); i++) {
+		histogram[i] = gaussian_pdf(i, mean, stddev);
+	}
+
+	vector<double> cdf(256, 0);
+	for (int i = 1; i < cdf.size(); i++) {
+		cdf[i] = cdf[i-1] + histogram[i];
+	}
+
+	return cdf;
+}
+
 
 vector<int> compute_histogram(PNG_Canvas_BW& image) {
 	vector<int> h(256, 0);
@@ -30,6 +52,7 @@ vector<int> compute_histogram(PNG_Canvas_BW& image) {
 	return h;
 }
 
+
 vector<int> compute_cumulative_histogram(PNG_Canvas_BW& image) {
 	vector<int> h = compute_histogram(image);
 	vector<int> H = h;
@@ -40,6 +63,7 @@ vector<int> compute_cumulative_histogram(PNG_Canvas_BW& image) {
 
 	return H;
 }
+
 
 void invert_image(PNG_Canvas_BW& image) {
 	int width = image.get_width();
@@ -58,22 +82,36 @@ void invert_image(PNG_Canvas_BW& image) {
 	image = outputImage;
 }
 
-void match_histogram(PNG_Canvas_BW& image, int Href[]) {
+
+vector<int> match_histogram(PNG_Canvas_BW& image, vector<int> Href, int nref = 256) {
 	int width = image.get_width();
 	int height = image.get_height();
+
 	int n = width * height;
+	int r = n / nref;
 	vector<int> h = compute_histogram(image);
-	
-	PNG_Canvas_BW outputImage(width, height);
-	
-	for (int x = 0; x < width; x++) {
-		for (int y = 0; y < height; y++) {
-			
+
+	vector<int> F(256, 0);
+	int i = 0;
+	int j = 0;
+	int c = 0;
+
+	while (i < 256) {
+		if (c <= r * Href[j]) {
+			c += h[i];
+			F[i] = j;
+			i++;
+		} else {
+			j++;
 		}
 	}
+	
+	// PNG_Canvas_BW outputImage(width, height);
+	// image = outputImage;
 
-	image = outputImage;
+	return F;
 }
+
 
 int main(int argc, char** argv) {
 	if (argc < 3) {
@@ -89,7 +127,9 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 	
+	// gaussian_cdf(0);
+	// cout << gaussian_pdf(0) << endl;
 	vector<int> Href = compute_cumulative_histogram(canvas);
-	// match_histogram(canvas, Href);
+	match_histogram(canvas, Href);
 	canvas.save_image(output_filename);
 }
