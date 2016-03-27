6011b641b3af934aa67e78bc77e842c29d3fe54c
MichaelReiter <michaelreiter@live.ca>
2016-03-26 17:24:05 -0700

    Add ripple mapping implementation

--- Assignment 5/ripple_mapping.cpp
+++ Assignment 5/ripple_mapping.cpp
@@ -30,6 +30,15 @@ double inverse_transform_y(int x, int y) {
 }
 
 
+int check_bounds(int x, int boundary) {
+  if (x < 0 || x >= boundary) {
+    return 128;
+  } else {
+    return x;
+  }
+}
+
+
 void apply_ripple(PNG_Canvas_BW& image, float scale = 3) {
   int width = image.get_width();
   int height = image.get_height();
@@ -38,7 +47,22 @@ void apply_ripple(PNG_Canvas_BW& image, float scale = 3) {
 
   for (int x = 0; x < width; x++) {
     for (int y = 0; y < height; y++) {
-      output_image[x][y];
+      int x_prime = inverse_transform_x(x, y);
+      int y_prime = inverse_transform_y(x, y);
+
+      x_prime = check_bounds(x_prime, width);
+      y_prime = check_bounds(y_prime, height);
+
+      int x0 = floor(x_prime);
+      int y0 = floor(y_prime);
+      int x1 = ceil(x_prime);
+      int y1 = ceil(y_prime);
+      float xs = x_prime - x0;
+      float ys = y_prime - y0;
+      float p0 = image[x0][y0] * (1 - xs) + image[x1][y0] * xs;
+      float p1 = image[x0][y1] * (1 - xs) + image[x1][y1] * xs;
+
+      output_image[x][y] = p0 * (1 - ys) + p1 * ys;
     }
   }
 
